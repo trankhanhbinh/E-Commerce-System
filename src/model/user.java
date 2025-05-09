a5c8635937625821ded1613c6b6f38a8a772e2fd
Trần Khánh Bình <tranbinh09122006@gmail.com>
2025-05-10 00:44:13 +0700

    add User class

--- src/model/user.java
+++ src/model/user.java
@@ -1,4 +1,53 @@
 package model;
-public class user {
-    
+public class User {
+    //constructor
+    private String userId;
+    private String userName;
+    private String userPassword;
+    private String userRegisterTime;
+    private String userRole;
+
+    public User(String userId, String userName, String userPassword, String userRegisterTime, String userRole) {
+        if (!userId.matches("^u_\\d{10}$")){
+            throw new IllegalArgumentException("Invalid userId format. Expected: u_ followed by 10 digits.");
+        }
+        this.userId = userId;
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userRegisterTime = userRegisterTime;
+        this.userRole = userRole;
+    }
+
+    public User(){
+        this.userRole = "customer";
+    }
+
+    // getter and setter (if needed)
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public String getUserRegisterTime() {
+        return userRegisterTime;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+    // toString method
+    @Override
+    public String toString(){
+        return String.format(
+            "{\"user_id\":\"%s\", \"user_name\":\"%s\", \"user_password\":\"%s\", " +
+            "\"user_register_time\":\"%s\", \"user_role\":\"%s\"}",
+            userId, userName, userPassword, userRegisterTime, userRole);
+    }
 }
