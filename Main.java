9aed316c28593169d7bac295e115488a22ed8b60
Trần Khánh Bình <tranbinh09122006@gmail.com>
2025-05-13 14:57:09 +0700

    update

--- Main.java
+++ Main.java
@@ -1,27 +1,6 @@
-<<<<<<< HEAD
-import java.util.ArrayList;
 import java.util.List;
-import data.UserFileWriter;
-import model.Admin;
-import model.User;
-
-public class Main {
-    public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-
-        users.add(new User("u_1234567890", "Alice", "pass123", "10-05-2025_12:30:00", "customer"));
-        users.add(new User("u_0987654321", "Bob", "secret", "09-05-2025_15:00:00", "customer"));
-        users.add(new Admin());        
-        UserFileWriter.writeUsersToFile(users);
-
-    }
-}
-=======
-package Assignment;
-
-import java.util.List;
-import Assignment.src.Interfaceio.IOInterface;
-import Assignment.src.operation.UserOperation;
+import Interfaceio.IOInterface;
+import operation.UserOperation;
 
 public class Main{
     public static void main(String[] args){
