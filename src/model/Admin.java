ba59f7c8f94bbd3d6be06488566dbb153f7c7cf9
Noobovich1 <siper.chicken@gmail.com>
2025-05-18 18:38:47 +0700

    Update Admin.java

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -2,10 +2,16 @@ package model;
 
 public class Admin extends User{
     //constructors
-    public Admin(String userId, String userName, String userPassword, String userRegisterTime, String userRole){
-        super(userId, userName, userPassword, userRegisterTime, userRole);
+    /**
+    * fix a bug in updating profile by adding isLoading
+    * for example like username, once you log out and log in again with
+    * the new username it will result in error and crash the run
+    * despite successfully saved the data into the .txt file
+    * -Khoa-
+    */
+    public Admin(String userId, String userName, String userPassword, String userRegisterTime, String userRole, boolean isLoading) {
+        super(userId, userName, userPassword, userRegisterTime, userRole, isLoading);
     }
-
     public Admin(){
         super();
         super.setUserRole("admin");
