f8bde2123e0e4f0457df46bdf06f27b9573e4a40
Noobovich1 <siper.chicken@gmail.com>
2025-05-19 00:19:51 +0700

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,10 +1,10 @@
-package Assignment.src.model;
+package model;
 
 public class Customer extends User {
     private String userEmail;
     private String userMobile;
 
-    // Original constructor for registration
+    // Original constructor
     public Customer(String userId, String userName, String userPassword, 
             String userRegisterTime, String userRole, String userEmail, String userMobile) {
         super(userId, userName, userPassword, userRegisterTime, userRole);
